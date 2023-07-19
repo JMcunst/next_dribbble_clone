403bae58be4daee2301c7602dc6fd44142546988
jmcunst <xnslqjtmghaf@gmail.com>
2023-07-19 15:51:28 +0900

    style: update eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
