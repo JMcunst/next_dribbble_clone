0698ed37079fb2330d93e9f71dbc3cdc9755a289
jmcunst <xnslqjtmghaf@gmail.com>
2023-07-19 19:01:20 +0900

    feat: make create project route

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { redirect } from "next/navigation";
 
-const page = () => {
+import { getCurrentUser } from "@/lib/session";
+import Modal from "@/components/Modal";
+import ProjectForm from "@/components/ProjectForm";
+
+const CreateProject = async () => {
+  const session = await getCurrentUser();
+
+  if (!session?.user) redirect("/")
+
   return (
-    <div>page</div>
-  )
-}
+    <Modal>
+      <h3 className="modal-head-text">Create a New Project</h3>
 
-export default page
+      <ProjectForm type="create" session={session} />
+    </Modal>
+  );
+};
+
+export default CreateProject;
