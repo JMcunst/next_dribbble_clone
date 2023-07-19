8976121c649e6755bf7d872c8ef8a2b63228c7ee
jmcunst <xnslqjtmghaf@gmail.com>
2023-07-19 17:12:57 +0900

    feat: make form create project

---
+++ components/CustomMenu.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import Image from 'next/image';
+
+type Props = {
+    title: string,
+    state: string,
+    filters: Array<string>,
+    setState: (value: string) => void;
+}
+
+const CustomMenu = ({ title, state, filters, setState }: Props) => {
+  return (
+    <div>CustomMenu</div>
+  )
+}
+
+export default CustomMenu
